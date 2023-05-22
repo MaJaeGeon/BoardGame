3d0f4fbd0900b1fbeefb1ca132bb6c1cf6ff02cb
MaJaeGeon <akworjs0517@gmail.com>
2023-05-22 20:43:29 +0900

    임시저장

--- BoardGame/Models/SpaceModel.cs
+++ BoardGame/Models/SpaceModel.cs
@@ -1,8 +1,17 @@
 ﻿namespace BoardGame.Models {
-    public class SpaceModel {
+    public interface ISpaceModel {
         public int SpaceId { get; set; }
-        public bool IsEventSpace { get; set; }
-        public bool IsSolved { get; set; }
+    }
+
+    public class EventSpaceModel : ISpaceModel {
+        public int SpaceId { get; set; }
+
+    }
+
+    public class QuizSpaceModel : ISpaceModel {
+        public int SpaceId { get; set; }
         public string? QuizPath { get; set; }
+        public int QuizReward { get; set; }
+        public bool IsSloved { get; set; }
     }
 }
--- BoardGame/Services/GameManagerService.cs
+++ BoardGame/Services/GameManagerService.cs
@@ -1,12 +1,17 @@
 ﻿using BoardGame.Models;
 using BoardGame.Utils;
+using System;
 
 namespace BoardGame.Services {
     public class GameManagerService {
         public List<TokenModel> Tokens { get; set; } = new List<TokenModel>();
-        public List<SpaceModel> Spaces { get; set; } = new List<SpaceModel>();
+        public List<ISpaceModel> Spaces { get; set; } = new List<ISpaceModel>();
 
         public bool IsModalOpen { get; set; }
+        public bool IsRolling { get; set; }
+
+
+        public ISpaceModel? CurrentSpace { get; set; }
 
         private int order = 0;
         /// <summary>
@@ -32,26 +37,49 @@ namespace BoardGame.Services {
             // Spaces
             Random random = new Random();
 
-            //1~47
-            var Quizzes = Enumerable.Range(1, 47).Select(i => $"images/space-bg/{i}.png").ToList().Shuffle();
 
-            //0~48 : 49개
-            Spaces = Enumerable.Range(0, 49).Select(i => {
-                var space = new SpaceModel {
+            /*
+            01~05 파란색
+            06~17 초록색
+            18~32 빨간색
+            33~47 노란색
+            */
+            var quizzes = Enumerable.Range(1, 47).Select(i => new { 
+                Path = $"images/space-bg/{i}.png", 
+                Reword = i switch {
+                    <= 5 => 3,
+                    >= 6 and <= 17 => 1,
+                    >= 18 and <= 32 => 2,
+                    >= 33 => 0
+            }}).ToList().Shuffle();
+
+            var quizSpaces = Enumerable.Range(1, 47).Select(i => {
+                var space = new QuizSpaceModel {
                     SpaceId = i,
-                    IsEventSpace = random.Next() % 4 == 0,
-                    QuizPath = !(i < 1 || i > 47) ? Quizzes[i - 1] : ""
+                    QuizPath = quizzes[i - 1].Path,
+                    QuizReward = quizzes[i - 1].Reword,
                 };
 
                 return space;
             }).ToList();
+
+            var firstSpace = new EventSpaceModel { SpaceId = 0 };
+            var lastSpace = new EventSpaceModel { SpaceId = 48 };
+
+            Spaces.Add(firstSpace);
+            Spaces.AddRange(quizSpaces);
+            Spaces.Add(lastSpace);
         }
+
 
         /// <summary>
         /// 토큰의 위치를 이동시킨다.
         /// </summary>
         /// <param name="count">이동시킬 칸 수</param>
         public async Task MoveToken(int count) {
+            if(IsRolling) return;
+            IsRolling = true;
+
             var order = Order;
             var token = Tokens.Find(t => t.Id == order) ?? throw new Exception();
 
@@ -62,9 +90,19 @@ namespace BoardGame.Services {
 
             // 토큰이 도착했을 때 퀴즈모달을 띄우고
             // 모달에는 퀴즈의 이미지 경로를 넘겨주고, 모달에서는 퀴즈 풀이 여부를 받아와야함.
-            var space = Spaces.Find(s => s.SpaceId == token.SpaceId);
-            Console.WriteLine(space.QuizPath);
-            //OpenModal(space.QuizPath);
+            CurrentSpace = Spaces.Find(s => s.SpaceId == token.SpaceId);
+
+            // 해결된 문제라면 모달을 띄우지 않음.
+            if(CurrentSpace is QuizSpaceModel quizSpace && quizSpace.IsSloved) return;
+
+            OpenModal();
+        }
+
+        public void QuizSlove(int point) {
+            var token = Tokens.Find(t => t.Id == order) ?? throw new Exception();
+            token.Point += point;
+
+            Console.WriteLine($"{token.Id} has {token.Point} Points!");
         }
 
 
